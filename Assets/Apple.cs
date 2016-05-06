a9b5f4c2e45f96edd1d19004a7c65c6dfdc0e1b2
Tim Murphy <j2amus@gmail.com>
2016-05-05 23:41:45 -0400

    Added Files

---
+++ Assets/Apple.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+	public static float     bottomY = -15f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( transform.position.y < bottomY ) {
+			Destroy( this.gameObject );
+			// Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // 1
+			// Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed();                                      // 2
+		}
+	}
+}
